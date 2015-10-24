d31712acdf0919b717e9c9ec9474edf6750da82e
Jose Abelardo Gutierrez <jose.abelardo@admios-sa.com>
2015-10-23 20:38:17 -0500

    still more refactoring

--- src/V2D.h
+++ src/V2D.h
@@ -10,58 +10,74 @@
 struct V2D {
   float x;
   float y;
-
-  float lengthSquare() {
-    return x * x + y * y;
-  }
-
-  float length() {
-    return sqrtf(lengthSquare());
-  }
-
-  void normalize() {
-    float l = length();
-    // we never want to attempt to divide by 0
-    if (l > 0) {
-      *this *= 1 / l;
-    }
-  }
-
-  V2D operator+(const V2D &v2) const {
-    return V2D{x + v2.x, y + v2.y};
-  }
-
-  V2D &operator+=(const V2D &v2) {
-    *this = *this + v2;
-    return *this;
-  }
-
-  V2D operator*(float scalar) {
-    return V2D{x * scalar, y * scalar};
-  }
-
-  V2D &operator*=(float scalar) {
-    *this = *this * scalar;
-    return *this;
-  }
-
-  V2D operator-(const V2D &v2) const {
-    return V2D{x - v2.x, y - v2.y};
-  }
-
-  V2D &operator-=(const V2D &v2) {
-    *this = *this - v2;
-    return *this;
-  }
-
-  V2D operator/(float scalar) {
-    return V2D{x / scalar, y / scalar};
-  }
-
-  V2D &operator/=(float scalar) {
-    *this = *this / scalar;
-    return *this;
-  }
 };
+
+float
+lengthSquare(const V2D& v2D) {
+  return v2D.x * v2D.x + v2D.y * v2D.y;
+}
+
+float
+length(const V2D& v2D) {
+  return sqrtf(lengthSquare(v2D));
+}
+
+V2D
+operator*(float scalar, const V2D& v2D) {
+  return V2D{v2D.x * scalar, v2D.y * scalar};
+}
+
+V2D
+operator*(const V2D& v2D, float scalar) {
+  return V2D{v2D.x * scalar, v2D.y * scalar};
+}
+
+V2D &
+operator*=(V2D& v2D, float scalar) {
+  v2D = v2D * scalar;
+  return v2D;
+}
+
+void
+normalize(V2D& v2D) {
+  float l = length(v2D);
+  // we never want to attempt to divide by 0
+  if (l > 0) {
+    v2D *= 1 / l;
+  }
+}
+
+V2D
+operator+(const V2D &v2a, const V2D &v2b) {
+  return V2D{v2a.x + v2b.x, v2a.y + v2b.y};
+}
+
+V2D &
+operator+=(V2D &v2a, const V2D &v2b) {
+  v2a = v2a + v2b;
+  return v2a;
+}
+
+V2D
+operator-(const V2D &v2a, const V2D &v2b) {
+  return V2D{v2a.x - v2b.x, v2a.y - v2b.y};
+}
+
+V2D &
+operator-=(V2D &v2a, const V2D &v2b) {
+  v2a = v2a - v2b;
+  return v2a;
+}
+
+V2D
+operator/(const V2D &v2, float scalar) {
+  return V2D{v2.x / scalar, v2.y / scalar};
+}
+
+V2D &
+operator/=(V2D &v2, float scalar) {
+  v2 = v2 / scalar;
+  return v2;
+}
 
 #endif //HELLO2SDL_V2D_H
